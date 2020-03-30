a83daa29f74470fa7e0a070c4ee8fa2b98afc596
Rym Rabehi <rym_rabehi@ultimatesoftware.com>
2020-03-30 09:26:04 +0200

    Update configuration

--- lisp/bash-completion.el
+++ lisp/bash-completion.el
@@ -1,4 +1,12 @@
-version 2 of the
+;;; bash-completion.el --- BASH completion for the shell buffer -*- lexical-binding: t -*-
+
+;; Copyright (C) 2009 Stephane Zermatten
+
+;; Author: Stephane Zermatten <szermatt@gmx.net>
+
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of the
 ;; License, or (at your option) any later version.
 
 ;; This program is distributed in the hope that it will be useful,
